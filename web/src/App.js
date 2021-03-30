90cdd20ca4925168e33b5f779bc1c88f9a663fd7
40259507 <pmurphy710@qub.ac.uk>
2021-03-30 18:30:00 +0100

    work in progress

--- web/src/App.js
+++ web/src/App.js
@@ -11,72 +11,85 @@ import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 
 function App() {
-  // const [data, setData] = useState([{"id": 1, "employee_id": 6298, "month": "January", "exercise_time": 224, "social_interaction_time": 60, "work_time": 2220, "sleep_time": 2100}, {"id": 2, "employee_id": 6299, "month": "January", "exercise_time": 224, "social_interaction_time": 60, "work_time": 2220, "sleep_time": 2100}]);
+	// const [data, setData] = useState([{"id": 1, "employee_id": 6298, "month": "January", "exercise_time": 224, "social_interaction_time": 60, "work_time": 2220, "sleep_time": 2100}, {"id": 2, "employee_id": 6299, "month": "January", "exercise_time": 224, "social_interaction_time": 60, "work_time": 2220, "sleep_time": 2100}]);
 
-  const useStyles = makeStyles({
-    root: { width: '80%', overflowY: "auto" },
-    table: {
-      minWidth: 650,
-      height: '500px',
-      overflowY: "hidden",
-      top: "100px"
-    },
-  });
+	const useStyles = makeStyles({
+		root: { width: '80%', overflowY: "auto" },
+		table: {
+			minWidth: 650,
+			height: '500px',
+			overflowY: "hidden",
+			top: "100px"
+		},
+	});
 
-  const classes = useStyles();
+	const classes = useStyles();
 
+	const [selectedMonth, setSelectedMonth] = useState(0)
+	console.log(selectedMonth)
 
-  const [data, setData] = useState([])
-  const [dataLoad, setDataLoad] = useState(false)
-  const url = 'http://localhost:5000/'
-  useEffect(async () => {
-    const result = await axios({
-      url: url,
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      },
-      responseType: 'json',
-    })
+	const months = function () {
+		const arrMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+			"November", "December"]
+		return arrMonths.map((name, index) => {
+			let selectedColor = index != selectedMonth ? "#3f51b5" : "#DC143C"
+			return (
+				<Button style={{ color: selectedColor }} onClick={() => { setSelectedMonth(index) }} >{name}</Button>
+			)
+		})
+	}
 
-    setData(result.data)
-    console.log(data)
-    setDataLoad(true);
-  }, [dataLoad]);
+	const [data, setData] = useState([])
+	const [dataLoad, setDataLoad] = useState(false)
+	const url = 'http://localhost:5000/per_employee/6299'
+	useEffect(async () => {
+		const result = await axios({
+			url: url,
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*'
+			},
+			responseType: 'json',
+		})
 
-  const months = function() {
-    const arrMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
-      "November", "December"]
-    return arrMonths.map((name, index) => {
-      return (
-        <Button style={{ color: "#3f51b5" }}>{name}</Button>
-      )})
-  }
+		setData(result.data)
+		console.log(data)
+		setDataLoad(true);
+	}, [dataLoad]);
 
-  return (
-    <div className="App">
+	return (
+		<div className="App">
 
-      <ButtonAppBar />
-		  <TextField id="standard-basic" label="Employee ID" />
-      <Grid container spacing={1}>
-        <Grid item xs={12} sm={6} className={classes.table}>
-          <BasicTable data={data} />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Typography variant="h4">
-            Progress Chart
+			<ButtonAppBar />
+			<Grid container justify="center" spacing={1}>
+				<Grid item xs={3}>
+					<TextField id="standard-basic" label="Employee ID" />
+				</Grid>
+				<Grid item xs={1} align="left">
+					<Button variant="contained" color="primary">
+						Show
+					</Button>
+				</Grid>
+			</Grid>
+			<Grid container spacing={1}>
+				<Grid item xs={12} sm={6} className={classes.table}>
+					<BasicTable data={data} />
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<Typography variant="h4">
+						Progress Chart
       </Typography>
-          <Example />
-        </Grid>
-      </Grid>
-      <CustomizedTimeline />
+					<Example data={data} />
+				</Grid>
+			</Grid>
+			<CustomizedTimeline />
 
-      <ButtonGroup size="large" color="primary" aria-label="large outlined primary button group">
-        {months()}
-      </ButtonGroup>
-    </div>
-  );
+			<ButtonGroup size="large" color="primary" aria-label="large outlined primary button group">
+				{months()}
+			</ButtonGroup>
+		</div>
+	);
 }
 
 export default App;
