130d1f401d602a4b3f8a89abfa1949eb79fa1126
Joel <jem@joelmcilroy.co.uk>
2021-03-30 18:14:56 +0100

    PieCharts

--- web/src/App.js
+++ web/src/App.js
@@ -8,6 +8,7 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
+import { PieChart } from 'react-minimal-pie-chart';
 
 function App() {
   // const [data, setData] = useState([{"id": 1, "employee_id": 6298, "month": "January", "exercise_time": 224, "social_interaction_time": 60, "work_time": 2220, "sleep_time": 2100}, {"id": 2, "employee_id": 6299, "month": "January", "exercise_time": 224, "social_interaction_time": 60, "work_time": 2220, "sleep_time": 2100}]);
@@ -61,6 +62,57 @@ function App() {
         <Example/>
         </Grid>
         </Grid>
+        <Grid container spacing={1}>
+        <Grid item xs={16} sm={4}>
+        <Typography variant="h4">
+        Work Life Balance Over a Year
+      </Typography>
+        <PieChart
+      data={[
+        { title: 'Work Time', value: 12390, color: '#DC143C' },
+        { title: 'Exercise Time', value: 2297, color: '#8884d8' },
+        { title: 'Sleep Time', value: 12727.5, color: '#82ca9d' },
+        { title: 'Social Time', value: 947, color: '#3f51b5' },
+      ]}
+      viewBoxSize={[100, 100]}
+      paddingAngle={[5]}
+      radius={[40]}
+    />;
+    </Grid>
+    <Grid item xs={16} sm={4}>
+    <Typography variant="h4">
+        Work Life Balance
+      </Typography>
+        <PieChart
+      data={[
+        { title: 'Work', value: 1, color: '#DC143C' },
+        { title: 'Exercise', value: 5, color: '#8884d8' },
+        { title: 'Sleep', value: 3, color: '#82ca9d' },
+        { title: 'Social', value: 3, color: '#3f51b5' },
+      ]}
+      viewBoxSize={[100, 100]}
+      paddingAngle={[5]}
+      radius={[40]}
+    />;
+    </Grid>
+    <Grid item xs={16} sm={4}>
+    <Typography variant="h4">
+        Work Life Balance Medians
+      </Typography>
+        <PieChart
+      data={[
+        { title: 'Work', value: 1, color: '#DC143C' },
+        { title: 'Exercise', value: 5, color: '#8884d8' },
+        { title: 'Sleep', value: 3, color: '#82ca9d' },
+        { title: 'Social', value: 3, color: '#3f51b5' },
+      ]}
+      viewBoxSize={[100, 100]}
+      paddingAngle={[5]}
+      radius={[40]}
+    />;
+    </Grid>
+    </Grid>
+    
         <CustomizedTimeline/>
     </div>
   );
